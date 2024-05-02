5f17d7d0e890b20c90e53ca7a28cbb8050640b21
sicalo330 <noah.esteban9@gmail.com>
2024-05-02 14:16:09 -0500

    Obtener todos los datos de la fakeApi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ConnectionService } from './service/connection.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'fakeApi';
+
+  constructor(private connection:ConnectionService){}
+
+  ngOnInit(): void {
+    this.connection.getData().subscribe(data => {
+      console.log(data)
+    })
+  }
 }
